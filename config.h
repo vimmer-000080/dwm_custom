0253a89251a7c53bc7c2d83afd710f85ca0a9893
vim_guy <none@none.com>
2024-10-09 06:25:47 +0530

    now this has added stuff

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Meslo LG L for Powerline:style=Bold:size=14" };
+static const char dmenufont[]       = "CommitMono Nerd Font:style=Bold:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -64,7 +64,7 @@ static const char *termcmd[]  = { "st", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,             XK_Return, spawn,          {.v = SHCMD("roxterm -e fish") } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = SHCMD("roxterm -e fish") } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = SHCMD("firefox-esr") } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
