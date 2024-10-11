983d9ffe033470090f3992d3cb6769cb56bd529e
vim_guy <none@none.com>
2024-10-11 14:42:39 +0530

    default colorscheme

--- .ccls-cache/@home@idiot@dwm_custom/config.h
+++ .ccls-cache/@home@idiot@dwm_custom/config.h
@@ -1,11 +1,12 @@
 /* See LICENSE file for copyright and license details. */
+#include <X11/XF86keysym.h>  // Include this for XF86 key symbols
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Meslo LG L for Powerline:style=Bold:size=14" };
+static const char *fonts[]          = { "CommitMono Nerd Font Propo:style=Regular:size=13" };
 static const char dmenufont[]       = "CommitMono Nerd Font:style=Italic:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -17,12 +18,23 @@ static const char col_green[]       = "#69F2B9";
 static const char col_purple[]      = "#5F6181";
 static const char col_yellow[]      = "#98871F";
 
+/* static const char *colors[][3]      = { */
+/* 	/1*               fg         bg         border   *1/ */
+/* 	[SchemeNorm] = { col_green, col_purple, col_cyan }, */
+/* 	[SchemeSel]  = { col_gray4, col_cyan_def,  col_yellow}, */
+/* }; */
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
+static const char col_brown[]       = "#361a09";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_green, col_purple, col_cyan },
-	[SchemeSel]  = { col_gray4, col_cyan_def,  col_yellow},
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeTitle] = { col_gray4, col_cyan,  col_cyan },
 };
-
 /* tagging */
 static const char *tags[] = { " ", "", "", "", "5", "6", "7", "8", "9" };
 
@@ -71,7 +83,6 @@ static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = SHCMD("st -e fish") } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = SHCMD("firefox-esr") } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
@@ -97,7 +108,8 @@ static const Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_c,      spawn,         {.v = SHCMD("antixcc.sh")} },
 	{ MODKEY,                       XK_e,      spawn,         {.v = SHCMD("tilix")} },
-	{ MODKEY,                       XK_r,      spawn,         {.v = SHCMD("rofi -show drun")} },
+	{ 0     ,                    XF86XK_MenuKB,spawn,         {.v = SHCMD("rofi -show run drun window -theme ~/.config/rofi/solarized.rasi")} },
+	{ 0     ,                       XK_Menu,   spawn,         {.v = SHCMD("rofi -show drun")} },
 	{ MODKEY,                       XK_u,      spawn,         {.v = SHCMD("firejail --noprofile pavucontrol")} },
 	{ MODKEY,                       XK_v,      spawn,         {.v = SHCMD("xfce4-terminal -e vim")} },
 	{ MODKEY,                       XK_y,      spawn,         {.v = SHCMD("st")} },
